c6993bbfea69428aa2391bba6ed898e0e670bf43
Mark Lester <marklester10.mlsm@gmail.com>
2020-04-05 16:36:15 +0800

    Added thank you view

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -26,7 +26,7 @@ class SurveyController extends Controller
         $survey = $questionnaire->surveys()->create($data['survey']);
         $survey->responses()->createMany($data['responses']);
 
-        return 'Thank you';
+        return view('/thankyou');
 
     }
 }
